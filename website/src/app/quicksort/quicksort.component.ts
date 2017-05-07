76a9ca818b050287caabd6c7f7869158b89aba51
Aleksandar Mitic <mitic.alex@hotmail.se>
2017-05-07 23:47:39 +0200

    Beginning of quicksort component

--- website/src/app/quicksort/quicksort.component.ts
+++ website/src/app/quicksort/quicksort.component.ts
@@ -1,48 +1,58 @@
-import {Component, OnInit} from '@angular/core';
+import {AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-quicksort',
   templateUrl: './quicksort.component.html',
   styleUrls: ['./quicksort.component.css']
 })
-export class QuicksortComponent implements OnInit {
-  list: number[] = [this.randomInt(), this.randomInt(), this.randomInt(), this.randomInt()
-    , this.randomInt(), this.randomInt(), this.randomInt(), this.randomInt(), this.randomInt(), this.randomInt()];
+export class QuicksortComponent implements OnInit, AfterViewInit {
+  context: CanvasRenderingContext2D;
+  @ViewChild('myCanvas') myCanvas;
+
+  heights: number[] = [60, 55, 50, 45, 40, 35, 30, 25, 20];
 
   constructor() {
-
-    /*
-      setTimeout(function(){
-        let staples = [document.getElementById('staple0'), document.getElementById('staple1'),
-          document.getElementById('staple2'), document.getElementById('staple3'),
-          document.getElementById('staple4'), document.getElementById('staple5'),
-          document.getElementById('staple6'), document.getElementById('staple7'),
-          document.getElementById('staple8'), document.getElementById('staple9')];
-
-
-        for (let i = 0; i <= staples.length; i++) {
-
-          let j = i;
-          while (j > 0 && staples[j - 1].style.marginTop < staples[j].style.marginTop) {
-
-            const tempMar = staples[j].style.marginTop;
-            const tempHeight = staples[j].style.height;
-
-            staples[j].style.marginTop = staples[j - 1].style.marginTop.valueOf();
-            staples[j].style.height = staples[j - 1].style.height.valueOf();
-
-            staples[j - 1].style.marginTop = tempMar.valueOf();
-            staples[j - 1].style.height = tempHeight.valueOf();
-
-            j = j - 1;
-          }
-        }
-      }, 0);
-     */
   }
 
-  randomInt() {
-    return Math.floor(Math.random() * (28 - 3 + 1) + 3);
+  ngAfterViewInit() {
+    let canvas = this.myCanvas.nativeElement;
+    this.context = canvas.getContext('2d');
+
+    this.tick();
+  }
+  tick() {
+    let ctx = this.context;
+    ctx.clearRect(0, 0, 350, 225);
+
+    const width = 30;
+    ctx.fillStyle = '#000';
+
+    for (let i = 0; i <= this.heights.length; i++) {
+      let j = i;
+
+      while (j > 0 && this.heights[j - 1] > this.heights[j]) {
+        const temp = this.heights[j];
+        this.heights[j] = this.heights[j - 1];
+        this.heights[j - 1] = temp;
+        j = j - 1;
+        break;
+      }
+    }
+
+    const start = new Date().getTime(); 
+    let end = start;
+    while (end < start + 500) {
+      end = new Date().getTime();
+    }
+
+    for (let i = 0; i < 7; i++) {
+      ctx.fillRect(width * i, 225 - this.heights[i], width, this.heights[i]);
+    }
+
+    requestAnimationFrame(() => {
+      this.tick();
+    });
+
   }
 
   wait(ms) {
@@ -53,17 +63,18 @@ export class QuicksortComponent implements OnInit {
     }
   }
 
-  sort() {
-    for (let i = 0; i <= this.list.length; i++) {
-      let j = i;
 
-      while (j > 0 && this.list[j - 1] > this.list[j]) {
-        const temp = this.list[j];
-        this.list[j] = this.list[j - 1];
-        this.list[j - 1] = temp;
-        j = j - 1;
-      }
-    }
+  sort() {
+    // for (let i = 0; i <= this.list.length; i++) {
+    //   let j = i;
+    //
+    //   while (j > 0 && this.list[j - 1] > this.list[j]) {
+    //     const temp = this.list[j];
+    //     this.list[j] = this.list[j - 1];
+    //     this.list[j - 1] = temp;
+    //     j = j - 1;
+    //   }
+    // }
   }
   ngOnInit() {
   }
