09e090c39ea87ff5e58ce1890d26f11e7f0c6b3c
Aleksandar Mitic <mitic.alex@hotmail.se>
2017-05-21 23:36:31 +0200

    Trying to implement ordering

--- src/app/quicksort/quicksort.component.ts
+++ src/app/quicksort/quicksort.component.ts
@@ -6,9 +6,6 @@ import {AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
   styleUrls: ['./quicksort.component.css']
 })
 export class QuicksortComponent implements OnInit, AfterViewInit {
-  sortText = "hidden";
-  sorter = "hidden";
-
   context: CanvasRenderingContext2D;
   @ViewChild('myCanvas') myCanvas;
 
@@ -19,12 +16,20 @@ export class QuicksortComponent implements OnInit, AfterViewInit {
 
   restart = true;
 
+
   constructor() {
+    window.addEventListener("load", function () {
+      const start = new Date().getTime();
+      let end = start;
+      while (end < start + 3000) {
+        end = new Date().getTime();
+      }
+      let test = document.getElementById("test")
+      test.style.visibility = "hidden";
+    });
   }
 
   ngAfterViewInit() {
-    this.wait(1000)
-    this.sorter = "visible"
     let canvas = this.myCanvas.nativeElement;
     this.context = canvas.getContext('2d');
 
@@ -125,59 +130,6 @@ export class QuicksortComponent implements OnInit, AfterViewInit {
     this.heights[firstIndex] = this.heights[secondIndex];
     this.heights[secondIndex] = temp;
   }
-
-  partition ( l, h) {
-    let x = this.heights[h];
-    let i = (l - 1);
-
-    for (let j = l; j <= h - 1; j++) {
-      if (this.heights[j] <= x) {
-        i++;
-        // swap arr[i] and arr[j]
-        this.swap(i, j);
-        return i + 1;
-      }
-    }
-    // swap arr[i+1] and arr[h]
-    this.swap(i + 1, h);
-    return (i + 1);
-}
-
-  quickSort(l, h) {
-    // create auxiliary stack
-    let stack = [];
-
-    // initialize top of stack
-    let top = -1;
-
-    // push initial values in the stack
-    stack[++top] = l;
-    stack[++top] = h;
-
-    // keep popping elements until stack is not empty
-    while (top >= 0) {
-      // pop h and l
-      h = stack[top--];
-      l = stack[top--];
-
-      // set pivot element at it's proper position
-      let p = this.partition(l, h);
-
-      // If there are elements on left side of pivot,
-      // then push left side to stack
-      if ( (p - 1) > l ) {
-        stack[++top] = l;
-        stack[++top] = p - 1;
-      }
-
-      // If there are elements on right side of pivot,
-      // then push right side to stack
-      if ((p + 1) < h ) {
-        stack[++top ] = p + 1;
-        stack[ ++top ] = h;
-      }
-    }
-}
 
   ngOnInit() {
   }
