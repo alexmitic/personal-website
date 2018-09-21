5b596250187c7d2b968eb79e32e4cfed75656b3f
Aleksandar Mitic <mitic.alex@hotmail.se>
2018-09-21 22:56:16 +0200

    Renamed arguments to some functions and reorderd the functions

--- src/app/learning/learning.component.ts
+++ src/app/learning/learning.component.ts
@@ -31,8 +31,6 @@ export class LearningComponent implements OnInit, AfterViewInit {
   //          |2|3|
 
   // Game values
-  playerX: number;
-  playerY: number;
   currGrid: number;
 
   LEARNING_RATE: number; // Learning rate is not readonly as it changes during the game
@@ -45,10 +43,7 @@ export class LearningComponent implements OnInit, AfterViewInit {
   constructor() {
 
     // Initiate game values
-    this.playerX = 0;
-    this.playerY = 0;
     this.currGrid = 0;
-
     this.success = 1;
 
     this.LEARNING_RATE = 1;
@@ -95,6 +90,9 @@ export class LearningComponent implements OnInit, AfterViewInit {
         this.qValues[i][j] = 0;
       }
     }
+  }
+
+  ngOnInit(): void {
   }
 
   ngAfterViewInit(): void {
@@ -172,12 +170,45 @@ export class LearningComponent implements OnInit, AfterViewInit {
     });
   }
 
-  getXGridCord(currGrid: number): number {
-    return this.gridToXpos(currGrid) * 25;
+  updateReward(nextDirr, nextGrid): void {
+    const r = this.REWARDS[this.currGrid][nextDirr];
+    const max = this.getMaxVal(nextGrid);
+    this.LEARNING_RATE = Math.pow(this.success, -0.1);
+
+    this.qValues[this.currGrid][nextDirr] = this.qValues[this.currGrid][nextDirr] + this.LEARNING_RATE * (r + this.DISCOUNT_FACTOR * max + this.qValues[this.currGrid][nextDirr]);
+
+    // this.qValues[this.currGrid][nextDirr] *= 1 - this.LEARNING_RATE;
+    // this.qValues[this.currGrid][nextDirr] += this.LEARNING_RATE * (r + this.DISCOUNT_FACTOR * max);
+
   }
 
-  getYGridCord(currGrid: number): number {
-    return this.gridToYpos(currGrid) * 25;
+  getMaxVal(grid): number {
+    const validDirr = this.getVaildDirr();
+
+    const values = this.qValues[grid];
+    let max = values[validDirr[0]];
+
+    for (let i = 1; i < validDirr.length; i++) {
+      if (values[validDirr[i]] > max) {
+        max = values[i];
+      }
+    }
+    return max;
+  }
+
+  fillRect(grid: number, color: string): void {
+    const ctx = this.context;
+    ctx.fillStyle = color;
+
+    ctx.fillRect(this.getXGridCord(grid), this.getYGridCord(grid), this.WIDTH, this.HEIGHT);
+  }
+
+  getXGridCord(grid: number): number {
+    return this.gridToXpos(grid) * 25;
+  }
+
+  getYGridCord(grid: number): number {
+    return this.gridToYpos(grid) * 25;
   }
 
   getNextDirection(): number {
@@ -198,18 +229,18 @@ export class LearningComponent implements OnInit, AfterViewInit {
     return direction;
   }
 
-  getMaxVal(currGrid): number {
-    const validDirr = this.getVaildDirr();
-
-    const values = this.qValues[currGrid];
-    let max = values[validDirr[0]];
-
-    for (let i = 1; i < validDirr.length; i++) {
-      if (values[validDirr[i]] > max) {
-        max = values[i];
+  gridToYpos(grid: number): number {
+    let yPos = 0;
+    for (let i = 8; i < 49; i += 8) {
+      if (grid < i) {
+        return yPos;
       }
+      yPos += 1;
     }
-    return max;
+  }
+
+  gridToXpos(grid: number): number {
+    return grid % 8;
   }
 
   stepCorrection(direction): number {
@@ -224,45 +255,6 @@ export class LearningComponent implements OnInit, AfterViewInit {
     }
   }
 
-  gridToYpos(currGrid: number): number {
-    let yPos = 0;
-    for (let i = 8; i < 49; i += 8) {
-      if (currGrid < i) {
-        return yPos;
-      }
-      yPos += 1;
-    }
-  }
-
-  gridToXpos(currGrid: number): number {
-    return currGrid % 8;
-  }
-
-  wait(ms): void {
-    const start = new Date().getTime();
-    let end = start;
-    while (end < start + ms) {
-      end = new Date().getTime();
-    }
-  }
-
-  randomNum(high): number {
-    return Math.floor(Math.random() * (high + 1));
-  }
-
-  updateReward(nextDirr, nextGrid): void {
-    const r = this.REWARDS[this.currGrid][nextDirr];
-    const max = this.getMaxVal(nextGrid);
-    this.LEARNING_RATE = Math.pow(this.success, -0.1);
-
-    this.qValues[this.currGrid][nextDirr] = this.qValues[this.currGrid][nextDirr] + this.LEARNING_RATE * (r + this.DISCOUNT_FACTOR * max + this.qValues[this.currGrid][nextDirr]);
-
-    // this.qValues[this.currGrid][nextDirr] *= 1 - this.LEARNING_RATE;
-    // this.qValues[this.currGrid][nextDirr] += this.LEARNING_RATE * (r + this.DISCOUNT_FACTOR * max);
-
-  }
-
-  // TODO This method works in a stupid way
   getVaildDirr(): any[] {
     const validDirr = [];
     switch (this.currGrid) {
@@ -406,17 +398,17 @@ export class LearningComponent implements OnInit, AfterViewInit {
           validDirr.push(2);
           validDirr.push(3);
         } else if (this.currGrid > 39) {
-            validDirr.push(0);
-            validDirr.push(2);
-            validDirr.push(3);
+          validDirr.push(0);
+          validDirr.push(2);
+          validDirr.push(3);
         } else if (this.currGrid % 8 === 0) {
-            validDirr.push(0);
-            validDirr.push(1);
-            validDirr.push(3);
+          validDirr.push(0);
+          validDirr.push(1);
+          validDirr.push(3);
         } else if (this.currGrid % 7 === 0) {
-            validDirr.push(0);
-            validDirr.push(1);
-            validDirr.push(2);
+          validDirr.push(0);
+          validDirr.push(1);
+          validDirr.push(2);
         } else {
           validDirr.push(0);
           validDirr.push(1);
@@ -429,13 +421,15 @@ export class LearningComponent implements OnInit, AfterViewInit {
     return validDirr;
   }
 
-  fillRect(currGird: number, color: string): void {
-    const ctx = this.context;
-    ctx.fillStyle = color;
-
-    ctx.fillRect(this.getXGridCord(currGird), this.getYGridCord(currGird), this.WIDTH, this.HEIGHT);
+  wait(ms): void {
+    const start = new Date().getTime();
+    let end = start;
+    while (end < start + ms) {
+      end = new Date().getTime();
+    }
   }
 
-  ngOnInit(): void {
+  randomNum(high): number {
+    return Math.floor(Math.random() * (high + 1));
   }
 }
